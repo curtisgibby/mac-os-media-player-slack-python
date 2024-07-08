bb29034e0baf54645fffda0a14788b17e1b0d47b
Curtis Gibby <cgibby@bamboohr.com>
2024-07-08 15:48:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# MacOS media / Slack Integration
+
+## Why does this exist?
+
+I wanted to be able to push my now-listening status (including album art) to my company's Slack.
+
+## Sounds cool. What do I need to do?
+
+### 1. Install the dependencies
+
+```bash
+pip3 install -r ./requirements.txt
+brew install nowplaying-cli
+```
+
+### 2. Create and update your `config.json` file
+
+```bash
+cp config.default.json config.json
+```
+
+### Get Slack tokens
+
+Help coming soon
+
+#### Update `emoji-name` _(optional)_
+
+Replace `my-album-art` with your desired Slack emoji name
+
+#### Update `time-format` _(optional)_
+
+Replace `%X` with your desired [datetime formatting string](https://strftime.org/)
+
+### 3. Start your media player
+
+You need to be playing your media _before_ moving on to step 4.
+
+### 4. Run the track change listener script
+
+```bash
+python3 macos-media-player-track-change-to-slack.py
+```
+
+The script will attempt to:
+
+- save your album art as a Slack emoji and
+- set your status to the now-playing text including the artist and title and the album-art emoji
+
+```plaintext
+[✓] 15:00:57 → 15:04:30 [:my-album-art:] Now Playing: All-4-One - So Much In Love
+[✓] 15:04:28 → 15:08:47 [:radio:] Now Playing: Haley Reinhart - I Belong to You
+```
+
+If the script is unable to create the album-art emoji (because of a bad token, no local album art, whatever), it will try to set the status using a standard emoji instead, randomly picking one of the following:
+
+- :cd: (`:cd:`)
+- :headphones: (`:headphones:`)
+- :musical_note: (`:musical_note:`)
+- :notes: (`:notes:`)
+- :radio: (`:radio:`)
+
+## Whom should I thank?
+
+- Thanks to Jack Ellenberger (@jackellenberger) for his [":slack_on_fire:" article](https://medium.com/@jack.a.ellenberger/slack-on-fire-part-two-please-stop-rotating-my-user-token-replay-attacking-slack-for-emoji-fun-c87da4e54b03) and his emojme library (particularly [`emoji-add.js`](https://github.com/jackellenberger/emojme/blob/e076b58bbe310da154013b51f77d3e1047938983/lib/emoji-add.js#L79-L82)) for helping me figure out how to push an emoji to Slack's [undocumented `/api/emoji.add` endpoint](https://webapps.stackexchange.com/a/126154/35105).
+- Thanks to [Kirtan Shah](https://github.com/kirtan-shah) for his [`nowplaying-cli`](https://github.com/kirtan-shah/nowplaying-cli) utility that provides the media information under the hood.
+
+## Do you have any improvements planned?
+
+- Add artist and/or song title blacklists to prevent specific tracks from being sent to Slack.
+- Check the current play time to more accurately determine an end time for the Slack status (instead of assuming that the whole song still needs to be played).
+- Consolidate the Mac, [Windows](https://github.com/curtisgibby/winrt-slack-python), and [Linux](https://github.com/curtisgibby/mpris-slack-python) versions of this script (so that the OS-specific parts of the process are pulled out into modules, and the common stuff can all be run in a single python script).
+- If possible, get rid of the external dependency on `nowplaying-cli`. (If you know how to get this data from native Python APIs, please add an answer to [my Stack Overflow question](https://stackoverflow.com/questions/78609762/how-can-i-use-pythons-mpnowplayinginfocenter-to-get-current-song-information-fr).)
---
+++ macos-media-player-track-change-to-slack.py
@@ -0,0 +1,264 @@
+import asyncio
+import base64
+import calendar
+import datetime
+import json
+import random
+import requests
+import subprocess
+import os
+from PIL import Image
+from io import BytesIO
+from time import sleep
+from time import strftime
+
+def get_default_status_emoji():
+    return random.choice([
+        ':cd:',
+        ':headphones:',
+        ':musical_note:',
+        ':notes:',
+        ':radio:',
+    ])
+
+def get_local_file():
+    imageData = current_media_info['thumbnail']
+    if imageData is None or imageData == 'null' or imageData == '':
+        return False
+
+    try:
+        # Load image from BytesIO
+        im = Image.open(BytesIO(base64.b64decode(imageData)))
+        local_filename = 'album-art.jpg'
+        im.thumbnail((96, 96))
+        im.save(local_filename)
+    except Exception as error:
+        print('Error writing file: ' + str(error))
+        return False
+
+    return local_filename
+
+
+def delete_slack_emoji():
+    postBody = {
+        'token': slack_token,
+        'name': emoji_name,
+    }
+    
+    r = requests.post(
+        'https://slack.com/api/emoji.remove',
+        data = postBody,
+        cookies = {'d': slack_cookie_token}
+    )
+
+    if(r.ok):
+        parsed = json.loads(r.text)
+        if parsed['ok']:
+            return True
+        else:
+            return False
+    else:
+        r.raise_for_status()
+    
+    return False
+
+def ensure_slack_does_not_have_emoji():
+    r = requests.get(
+        'https://slack.com/api/emoji.list',
+        params = {'token': slack_token},
+        cookies = {'d': slack_cookie_token}
+    )
+
+    if(r.ok):
+        parsed = json.loads(r.text)
+        if parsed['ok']:
+            if emoji_name in parsed['emoji']:
+                return delete_slack_emoji()
+            else:
+                return True
+        else:
+            return False
+    else:
+        r.raise_for_status()
+    
+    return False
+
+def upload_file_to_slack(local_file):
+    slack_does_not_have_emoji = ensure_slack_does_not_have_emoji()
+    if not slack_does_not_have_emoji:
+        return False
+    with open(local_file, 'rb') as f:
+        postBody = {
+            'token': slack_token,
+            'mode': 'data',
+            'name': emoji_name,
+        }
+        
+        files = {'image': f}
+
+        r = requests.post(
+            'https://slack.com/api/emoji.add',
+            data = postBody,
+            files = files,
+            cookies = {'d': slack_cookie_token}
+        )
+    
+    if os.path.exists(local_file):
+        os.remove(local_file)
+    if(r.ok):
+        parsed = json.loads(r.text)
+        if parsed['ok']:
+            return emoji_name
+        else:
+            return False
+    else:
+        # r.raise_for_status() # debug!
+        return False
+
+def get_status_emoji():
+    local_file = get_local_file()
+    if local_file:
+        uploaded_file_to_slack = upload_file_to_slack(local_file)
+        if uploaded_file_to_slack:
+            return ':' + uploaded_file_to_slack + ':'
+
+    return get_default_status_emoji()
+
+def set_slack_status():
+    if current_media_info['artist'] == '' or current_media_info['title'] == '':
+        return False
+    status_text = 'Now Playing: ' + current_media_info['artist'][:50] + ('...' if len(current_media_info['artist']) > 50 else '') + ' - ' + current_media_info['title']
+    status_text = status_text[:97] + ('...' if len(status_text) > 97 else '')
+
+    status_emoji = get_status_emoji()
+
+    expiration_time = datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=current_media_info["length"])
+    local_expiration_time = datetime.datetime.now() + datetime.timedelta(seconds=current_media_info["length"])
+
+    profile = {
+        'status_emoji': status_emoji,
+        'status_expiration': calendar.timegm(expiration_time.timetuple()),
+        'status_text': status_text,
+    }
+
+    postBody = {
+        'token': slack_token,
+        'profile': json.dumps(profile),
+    }
+
+    print('[ ] \u001b[36m' + strftime(time_format) +
+          ' → ' + local_expiration_time.strftime(time_format) +
+          ' \u001b[33m[' + status_emoji + ']' +
+          ' \u001b[0m' + status_text, end='\r')
+
+    r = requests.post(
+        'https://bamboohr.slack.com/api/users.profile.set',
+        data = postBody,
+        cookies = {'d': slack_cookie_token}
+    )
+
+    if(r.ok):
+        parsed = json.loads(r.text)
+        if parsed['ok']:
+            print('[\u001b[32m✓\u001b[0m')
+        else:
+            print('[\u001b[30m×\u001b[0m')
+            print('Error setting status : ' + parsed['error'])
+            quit()
+    else:
+        r.raise_for_status()
+
+async def get_media_info():
+
+    # Command to execute
+    cmd = ["nowplaying-cli", "get", "artist", "title", "duration", "artworkData"]
+
+    # Execute the command
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    # Check if the command was executed without errors
+    if error is None:
+        [artist, title, duration, thumbnail, *junk] = output.decode('utf-8').split('\n')
+        if duration != 'null':
+            duration = int(float(duration))
+        else:
+            duration = 180
+    else:
+        print(f"Error occurred while executing command: {error}")
+
+    if artist == 'null' or title == 'null':
+        raise RuntimeError
+
+    return {
+        'artist': artist,
+        'title': title,
+        'length': duration,
+        'thumbnail': thumbnail
+    }
+
+try:
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+except IOError as error:
+    print('Unable to read `config.json` file')
+    quit()
+
+try:
+    slack_token = config['slack-token']
+except Exception as error:
+    print('Config value `slack-token` not defined in `config.json`')
+    quit()
+
+try:
+    slack_cookie_token = config['slack-cookie-token']
+except Exception as error:
+    print('Config value `slack-cookie-token` not defined in `config.json`')
+    quit()
+
+try:
+    time_format = config['time-format']
+except Exception as error:
+    print('Config value `time-format` not defined in `config.json`')
+    quit()
+
+try:
+    emoji_name = config['emoji-name']
+except Exception as error:
+    emoji_name = 'my-album-art'
+
+try:
+    if slack_token == '':
+        raise Exception('empty string')
+    if slack_token == 'YOUR_SLACK_TOKEN' :
+        raise Exception(slack_token)
+except Exception as error:
+    print('Invalid value for `slack-token` in `config.json`: ' + str(error))
+    quit()
+
+previous_media_info = {
+    'artist': '',
+    'title': ''
+}
+
+while True:
+    try:
+        current_media_info = asyncio.run(get_media_info())
+    except RuntimeError:
+        current_media_info = previous_media_info
+
+    if current_media_info is None:
+        current_media_info = previous_media_info
+
+    # Avoid sending advertisement titles as now-listening statuses
+    if any(x in current_media_info['title'] for x in (':05', ':06', ':15', ':30', ' | ')):
+        current_media_info = previous_media_info
+
+    if current_media_info['artist'] != previous_media_info['artist'] or current_media_info['title'] != previous_media_info['title']:
+        previous_media_info = current_media_info
+        set_slack_status()
+
+    try:
+        sleep(5)
+    except KeyboardInterrupt:
+        quit()
